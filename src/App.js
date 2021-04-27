e40086dc737165ffd51e9582e91734013433a5ea
anthony-tamarato <bskhacker@gmail.com>
2021-04-26 22:00:52 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
+//App.js
+import React from 'react';
+import Thumbnail from './components/thumbnail.js';
+
+// Import the BrowserRouter, Route and Link components
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Thumbnail
+      demo="images/php_api_test_1.jpg"
+      code="https://github.com/anthony-tamarato/anthony_tamarato_api_test"
+      image="images/php_api_test_1.jpg"
+      title="PHP/API Test"
+      description="A PHP test page that dynamicly creates a paginated table from an API call."
+       />
+
+      <Thumbnail
+      demo="https://kensehmanthejeweler.com/"
+      code="#"
+      image="images/jeweler.jpg"
+      title="Jeweler"
+      description="A redsigne for a jeweler. Created with WordPress."
+       />
+       <Thumbnail
+      demo="images/turf.jpg"
+      code="#"
+      image="images/turf.jpg"
+      title="Lawncare"
+      description="An ecommerce website for a truf company. Created with WordPress."
+       />
+       <Thumbnail
+      demo="https://jetservice-envelope.com/"
+      code="#"
+      image="images/letter.jpg"
+      title="Print Shop"
+      description="An ecommerce website for a print shop. Created with WordPress."
+       />
+       <Thumbnail
+      demo="https://heathershomeworks.com/"
+      code="#"
+      image="images/cleaner.jpg"
+      title="Home Cleaners"
+      description="A redesign for a home cleaners. Created with WordPress."
+       />
+    </>
   );
 }
 
---
+++ src/components/thumbnail.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import { CSSTransition } from 'react-transition-group';
+
+
+export class Thumbnail extends Component{
+    
+    constructor(props) {
+        super(props);
+        // Don't call this.setState() here!
+        this.state = { show: false };
+        this.showLightbox = this.showLightbox.bind(this);
+        this.closeLightBox = this.closeLightBox.bind(this);
+      }
+
+    showLightbox(){
+        this.setState({
+            show : true
+        });
+    }
+
+    closeLightBox(){
+        this.setState({
+            show : false
+        });
+    }
+
+    render(){
+        const { show } = this.state;
+        const { demo, code, title, image, description} = this.props;
+
+        return(
+            <>
+            <div className="col-4 col-6-medium col-12-small canClick" onClick={this.showLightbox}>
+                <h3 className="project-title">{title}</h3>
+                <div className="image fit">
+                    <img src={image} alt="Project Image"/>
+                </div>
+            </div>
+
+            <CSSTransition
+                in={show}
+                timeout={300}
+                unmountOnExit
+                classNames="alert"
+            > 
+                <div className="lightbox">
+                    <button onClick={this.closeLightBox} className="closeButton">X</button>
+                    <h2 className="project-title">{title}</h2>
+                    <p>{description}</p>
+                    <div>
+                        {demo !== "#" &&
+                            <a href={demo} className="button project-link" target="_blank">View</a>
+                        }
+                        {code !== "#" &&
+                            <a href={code} className="button project-link" target="_blank">Code</a>
+                        }
+                    </div>
+                </div>
+            </CSSTransition>
+            </>
+        );
+    }
+}
+
+export default Thumbnail
